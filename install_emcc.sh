067139e2b7d314f1f242448fced8c68f4b5d3ba1
Paul Taylor <60293767+Y0ursTruly@users.noreply.github.com>
2024-12-20 02:13:55 +0000

    wasm compilation FINALLY works >:D

--- install_emcc.sh
+++ install_emcc.sh
@@ -7,6 +7,4 @@ echo '. /workspaces/*/emsdk/emsdk_env.sh' >> $HOME/.bashrc
 
 
 # emcc pow.c -o takeTest.js -sEXPORTED_FUNCTIONS=_takeTest,_freeString -sEXPORTED_RUNTIME_METHODS=ccall,cwrap
-# gcc pow.c -lssl -lcrypto
-# emcc -c -o libtomcrypt.o src/*.c
-# emcc pow.c libtomcrypt.o -o takeTest.js -sEXPORTED_FUNCTIONS=_takeTest,_freeString -sEXPORTED_RUNTIME_METHODS=ccall,cwrap
+# the above command finally works :D
